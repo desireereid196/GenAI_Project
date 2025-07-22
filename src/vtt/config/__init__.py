574e797337ed4f5cc7cb672311ae9e6829bb0c30
Curtis Neiderer <curtis.neiderer@gmail.com>
2025-07-22 15:03:27 -0400

    created config sub-package and moved config.py from utils sub-package to it

---
+++ src/vtt/config/__init__.py
@@ -0,0 +1,10 @@
+# Make constants directly accessible via vtt.config
+from .config import (
+    START_TOKEN,
+    END_TOKEN,
+    OOV_TOKEN,
+    PAD_TOKEN,  # Include PAD_TOKEN if you have it
+    IMAGE_SIZE,
+    IMAGENET_MEAN,
+    IMAGENET_STD,
+)
---
+++ src/vtt/utils/__init__.py
@@ -0,0 +1,2 @@
+# Import functions from helpers.py to make them directly accessible via vtt.utils
+from .helpers import detect_and_set_device, set_seed, save_image_with_caption
